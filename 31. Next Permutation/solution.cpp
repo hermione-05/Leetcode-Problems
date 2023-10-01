d57ade203a79323548bd1837927607a7373e64b4
Abhipsa05 <abhipsap2003@gmail.com>
2023-10-01 16:00:19 +0530

    Adding new problem

---
+++ 31. Next Permutation/solution.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int> &nums)
+{
+    int n = nums.size(), k, l;
+    for (k = n - 2; k >= 0; k--)
+    {
+        if (nums[k] < nums[k + 1])
+        {
+            break;
+        }
+    }
+    if (k < 0)
+    {
+        reverse(nums.begin(), nums.end());
+    }
+    else
+    {
+        for (l = n - 1; l > k; l--)
+        {
+            if (nums[l] > nums[k])
+            {
+                break;
+            }
+        }
+        swap(nums[k], nums[l]);
+        reverse(nums.begin() + k + 1, nums.end());
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    nextPermutation(v);
+    for (int i = 0; i < n; i++)
+        cout << v[i] << " ";
+    return 0;
+}
